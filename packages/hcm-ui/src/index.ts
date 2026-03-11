276054f57f89e5a18207035ce8f5def42a588fff
Felix Pherry <182051.FELIX@klgroup.local>
2026-03-11 11:10:42 +0700

    chore(hcm-ui): update barrel exports for Phase 1 hooks

--- packages/hcm-ui/src/index.ts
+++ packages/hcm-ui/src/index.ts
@@ -1,6 +1,14 @@
-// export {
-//   useSelection,
-//   type UseSelectionOptions,
-//   type UseSelectionReturn,
-//   type SelectionState,
-// } from './hooks/use-selection';
+export {
+  useSelection,
+  type UseSelectionOptions,
+  type UseSelectionReturn,
+  type SelectionState,
+} from './hooks/use-selection';
+
+export {
+  useFieldVisibility,
+  type UseFieldVisibilityOptions,
+  type UseFieldVisibilityReturn,
+} from './hooks/use-field-visibility';
+
+export { useFilter, type UseFilterOptions, type UseFilterReturn } from './hooks/use-filter';
